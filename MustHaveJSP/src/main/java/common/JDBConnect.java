db733906273eaf50755128310700af5bb9eb7ded
loklok2 <loklok2@naver.com>
2024-06-20 17:59:27 +0900

    10rdweek/day4/ch8

--- MustHaveJSP/src/main/java/common/JDBConnect.java
+++ MustHaveJSP/src/main/java/common/JDBConnect.java
@@ -3,6 +3,7 @@ package common;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+
 import jakarta.servlet.ServletContext;
 
 public class JDBConnect {
@@ -12,18 +13,12 @@ public class JDBConnect {
 	private String pwd = "tiger";
 	private Connection con;
 	
-	public Connection getConnection() {
-        return con;
-    }
 
-    public void setConnection(Connection con) {
-        this.con = con;
-    }
     
 	public JDBConnect() {
         try {
             Class.forName(driver);
-            con = DriverManager.getConnection(url, id, pwd);
+            setCon(DriverManager.getConnection(url, id, pwd));
             System.out.println("DB 연결 성공(기본 생성자)");
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,8 +27,8 @@ public class JDBConnect {
 
     public void close() {
         try {
-            if (con != null && !con.isClosed()) {
-                con.close();
+            if (getCon() != null && !getCon().isClosed()) {
+                getCon().close();
                 System.out.println("JDBC 자원 해제");
             }
         } catch (Exception e) {
@@ -44,7 +39,7 @@ public class JDBConnect {
     public JDBConnect(String driver, String url, String id, String pwd) {
         try {
             Class.forName(driver);
-            con = DriverManager.getConnection(url, id, pwd);
+            setCon(DriverManager.getConnection(url, id, pwd));
             System.out.println("DB 연결 성공(인수 생성자1)");
         } catch (Exception e) {
             e.printStackTrace();
@@ -54,12 +49,20 @@ public class JDBConnect {
     public JDBConnect(ServletContext application) {
     	try {
             Class.forName(driver);
-            con = DriverManager.getConnection(url, id, pwd);
+            setCon(DriverManager.getConnection(url, id, pwd));
             System.out.println("DB 연결 성공(인수 생성자2)");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
 	
 	
 }
--- MustHaveJSP/src/main/java/membership/MemberDTO.java
+++ MustHaveJSP/src/main/java/membership/MemberDTO.java
@@ -1,12 +1,13 @@
 package membership;
 
 public class MemberDTO {
-
+	// 멤버 변수 선언
 	private String id;
 	private String pass;
 	private String name;
 	private String regidate;
 	
+	// 멤버 변수별 게터와 세터
 	public String getId() {
 		return id;
 	}
@@ -18,19 +19,25 @@ public class MemberDTO {
 	public String getPass() {
 		return pass;
 	}
+	
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getRegidate() {
 		return regidate;
 	}
+	
 	public void setRegidate(String regidate) {
-		this.regidate = regidate;
+		this.regidate =regidate;
 	}
+	
 }
